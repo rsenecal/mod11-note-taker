cf02146e831d242aa225cde1a12cea3cee4c4bdc
rsenecal <roodysenecal@gmail.com>
2022-10-28 18:14:50 -0400

    Getting the index page to load

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const noteData = require('./db/db.json');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
