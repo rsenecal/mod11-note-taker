27383369d4948320d0e7ceafc80ac1c00d2282e1
rsenecal <roodysenecal@gmail.com>
2022-10-28 23:05:45 -0400

    Add Note Taker Logo and fix wildcard route

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.get('/notes',  (req, res) => {
 });
 
 
+app.get('/api/notes', (req, res) => res.json(noteData));
+
 app.get('*',  (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-
-app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
