b6bd23963d17449bab8a73e4fdf1a77b1af1b261
rsenecal <roodysenecal@gmail.com>
2022-10-28 22:10:53 -0400

    Getting not page to load

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
-
+console.log(noteData);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -12,9 +12,16 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+
+app.get('/notes',  (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+
+
+app.get('*',  (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 app.get('/api/notes', (req, res) => res.json(noteData));
 
